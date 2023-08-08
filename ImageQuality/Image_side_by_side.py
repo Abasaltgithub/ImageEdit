283e0dc65e827f46497429dc5d20b044a0ac8deb
Abasalt Bahrami <83898640+Abasaltgithub@users.noreply.github.com>
2023-08-08 18:41:13 -0300

    update

---
+++ ImageQuality/Image_side_by_side.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Load images
+image_paths = [
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/CTR_0hr_1pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/CTR_5hr_6pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_5hr_6pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_5hr_6pm.png'
+]
+
+images = [mpimg.imread(image_path) for image_path in image_paths]
+
+# Display images in a 2x2 grid without spaces and titles
+plt.figure(figsize=(8, 8))  # Adjust the figure size as needed
+
+for i, image in enumerate(images, 1):
+    plt.subplot(2, 2, i)
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')  # Turn off axis
+    plt.subplots_adjust(wspace=0, hspace=0)  # Remove spaces
+
+plt.show()
