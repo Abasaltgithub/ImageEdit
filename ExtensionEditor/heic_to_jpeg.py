5d3480ca25dfcf972c8c8bdb8827e7bac3ec8819
Abasalt Bahrami <83898640+Abasaltgithub@users.noreply.github.com>
2023-08-21 20:40:52 -0700

    update

---
+++ ExtensionEditor/heic_to_jpeg.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+
+def convert_and_rename(source_folder, destination_folder):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    for filename in os.listdir(source_folder):
+        if filename.lower().endswith('.heic'):
+            heic_path = os.path.join(source_folder, filename)
+            jpeg_path = os.path.join(
+                destination_folder, os.path.splitext(filename)[0] + '.jpg')
+
+            # Convert HEIC to JPEG using heif-convert command-line tool
+            subprocess.run(["heif-convert", heic_path, jpeg_path])
+
+            # Remove the original HEIC file
+            os.remove(heic_path)
+
+
+if __name__ == "__main__":
+    heic_input_directory = "/Users/abasaltbahrami/Downloads/Receipts/heic_images"
+    jpeg_output_directory = "/Users/abasaltbahrami/Downloads/Receipts/jpeg_images"
+
+    convert_and_rename(heic_input_directory, jpeg_output_directory)
+    print("Conversion and renaming complete.")
--- ImageQuality/Image_side_by_side.py
+++ ImageQuality/Image_side_by_side.py
@@ -5,10 +5,13 @@ import matplotlib.image as mpimg
 image_paths = [
     '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/CTR_0hr_1pm.png',
     '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/CTR_5hr_6pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/CTR_18hr_12pm.png',
     '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/B_0hr_1pm.png',
     '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/B_5hr_6pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/B_18hr_12pm.png',
     '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_5hr_6pm.png',
-    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_5hr_6pm.png'
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_5hr_6pm.png',
+    '/Users/abasaltbahrami/My Drive/InCor - Brazil/CellMigration/Phone_18hr_12pm.png'
 ]
 
 images = [mpimg.imread(image_path) for image_path in image_paths]
@@ -17,7 +20,7 @@ images = [mpimg.imread(image_path) for image_path in image_paths]
 plt.figure(figsize=(8, 8))  # Adjust the figure size as needed
 
 for i, image in enumerate(images, 1):
-    plt.subplot(3, 2, i)
+    plt.subplot(3, 3, i)
     plt.imshow(image, cmap='gray')
     plt.axis('off')  # Turn off axis
     plt.gca().set_aspect('equal')  # Set equal aspect ratio
